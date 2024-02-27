5a3ad3a2fc45f9caf60233b23079b9ae1a883d24
Aniket222-001 <139674804+Aniket222-001@users.noreply.github.com>
2024-02-27 21:53:35 +0530

    Update Sliderdata.js

--- src/data/Sliderdata.js
+++ src/data/Sliderdata.js
@@ -3,7 +3,7 @@ const sliders = [
     id: 1,
     title: "Avengers",
     Category: 'Action',
-    img: require('../Components/Images/the-avengers-superhero-movie-eeotwqkmypkvalg9.jpg'),
+    img: 'https://res.cloudinary.com/dkb6cpj99/image/upload/v1709050886/MovieImages/oprckt2k783nh6euirlc.jpg',
     video:'https://res.cloudinary.com/dkb6cpj99/video/upload/v1709044250/Movievideos/Hollywood%20trailers/ygt8a5jrcujqtsspcuau.mp4',
     desc: `lLorem, ipsum dolor sit amet consectetur adipisicing elit. 
     Nulla laborum officiis numquam aut quod iusto deleniti, ullam molestias rem. Incidunt, si
@@ -17,7 +17,7 @@ const sliders = [
     id: 2,
     title: "Deadpool",
     Category: 'Action',
-    img: require('../Components/Images/Deadpool.jpg'),
+    img: 'https://res.cloudinary.com/dkb6cpj99/image/upload/v1709050887/MovieImages/s0wkovp3w6tmchnox2fn.jpg',
     video:'https://res.cloudinary.com/dkb6cpj99/video/upload/v1709044264/Movievideos/Hollywood%20trailers/mh0tavs1aag1lrgfi9g8.mp4',
     desc: `lLorem, ipsum dolor sit amet consectetur adipisicing elit. 
     Nulla laborum officiis numquam aut quod iusto deleniti, ullam molestias rem. Incidunt, si
@@ -30,7 +30,7 @@ const sliders = [
     id: 3,
     title: "Justice League",
     Category: 'Action',
-    img: require('../Components/Images/justice-league2751453.jpg'),
+    img: 'https://res.cloudinary.com/dkb6cpj99/image/upload/v1709050890/MovieImages/csg34yufn2bhsffkxi09.jpg',
     video:'https://res.cloudinary.com/dkb6cpj99/video/upload/v1709044257/Movievideos/Hollywood%20trailers/d6hrtfipbjlt5jreaibj.mp4',
     desc: `lLorem, ipsum dolor sit amet consectetur adipisicing elit. 
     Nulla laborum officiis numquam aut quod iusto deleniti, ullam molestias rem. Incidunt, si
